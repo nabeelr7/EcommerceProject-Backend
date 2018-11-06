6afe70b3e54870d1ef50c46057d15ffcc14ef070
Nabeel R <nabeelramzeen@gmail.com>
2018-11-06 12:24:15 -0500

    second

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
-let x = 5
+let express = require('express')
+let app = express()
+let bodyParser = require('body-parser')
+app.use(bodyParser.raw({ type: '*/*' }))
+
+
+app.get('/foobar', function(req, res) {
+	res.send("Hello world!")
+})
+app.post('/hasbody', function(req, res) {
+    res.send('You sent: ' + req.body.toString())
+ })
+ 
+
+app.listen(4000, function() { console.log("Server started on port 4000") })
