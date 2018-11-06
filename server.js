e8461e0cf1578911b7fba6ba48cdb482fe8a0228
Nabeel R <nabeelramzeen@gmail.com>
2018-11-06 13:59:39 -0500

    server

--- server.js
+++ server.js
@@ -3,13 +3,53 @@ let app = express()
 let bodyParser = require('body-parser')
 app.use(bodyParser.raw({ type: '*/*' }))
 
+let passwords = {}
+let sessions = {}
+let usersItems = {}
+let itemDescriptions = {}
 
-app.get('/foobar', function(req, res) {
-	res.send("Hello world!")
+const genID = () => { return Math.floor(Math.random() * 10000000000) }
+
+app.post("/signup", (req, res) => {
+    let parsed = JSON.parse(req.body)
+    let username = parsed.username
+    let password = parsed.password
+
+    if (passwords[username]) {
+        res.send(JSON.stringify(
+            { "success": "false" }
+        ))
+        return
+    }
+
+    passwords[username] = password
+    let sessionID = genID()
+    sessions[sessionID] = username
+    res.set('Set-Cookie', sessionID)
+    res.send(JSON.stringify(
+        { "success": "true" }
+    ))
 })
-app.post('/hasbody', function(req, res) {
-    res.send('You sent: ' + req.body.toString())
- })
- 
 
-app.listen(4000, function() { console.log("Server started on port 4000") })
+app.post("/login", (req, res) => {
+    let parsed = JSON.parse(req.body)
+    let username = parsed.username
+    let password = parsed.password
+
+    if (passwords[username] !== password) {
+        res.send(JSON.stringify(
+            { "success": "false" }
+        ))
+        return
+    }
+
+    let sessionID = genID()
+    sessions[username] = sessionID
+
+    res.send(JSON.stringify(
+        { "success": "true" }
+    ))
+})
+
+
+app.listen(4020, function () { console.log("Server started on port 4020") })
